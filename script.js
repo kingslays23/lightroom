63d0d0e07bf87fcb541f052a91554f65315b986e
kingslay <i.kkingslays@gmail.com>
2022-05-21 10:41:10 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMenu(){
+    document.getElementById("nav-links").style.display = "block";
+}
+function closeMenu(){
+    document.getElementById("nav-links").style.display = "none";
+}
+
+console.log(document.getElementById("nav-links"));
